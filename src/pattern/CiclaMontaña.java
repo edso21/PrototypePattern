32babd3287ba7e85b63e727c1132b489568dc004
Kuzzye <kevin.chepe@outlook.com>
2021-08-31 18:09:49 -0500

    Se agrega atributo de Suspención de Clase CiclaMontaña

--- src/pattern/CiclaMontaña.java
+++ src/pattern/CiclaMontaña.java
@@ -3,12 +3,13 @@ package pattern;
 
 public class CiclaMontaña extends Cicla{
  
-
+    
     public CiclaMontaña(){}
+    private String suspension;
     
     private CiclaMontaña(CiclaMontaña cm){
         super(cm);
-       
+       this.suspension=cm.suspension;
     }
     
     @Override
@@ -16,6 +17,14 @@ public class CiclaMontaña extends Cicla{
         return new CiclaMontaña(this);
     }
 
+    public String getSuspension() {
+        return suspension;
+    }
+
+    public void setSuspension(String suspension) {
+        this.suspension = suspension;
+    }
+    
 }
 
 
